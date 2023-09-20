9de5cc9704f2bc341d080d72c08e50a507b4b6e2
euklit <n.lieven@outlook.com>
2023-09-20 19:40:35 +0200

    Cleaning up

--- javascript.js
+++ javascript.js
@@ -4,19 +4,16 @@ grid.classList.add('grid');
 const body = document.querySelector('body')
 
 
-for (let i=0;i<16*16;i++) {
-    const box = document.createElement('div');
-    box.classList.add("box");
-    grid.appendChild(box);
+for (let i=0;i<16*16;i++) { 
+    const tile = document.createElement('div');
+    tile.classList.add("tile");
+    grid.appendChild(tile);
 }
-// console.log(body)
+
 body.appendChild(grid);
 
-const boxes = document.querySelectorAll('.box');
-boxes.forEach(box => box.addEventListener('mouseover', () => {
-    box.classList.add("hover");
+const tiles = document.querySelectorAll('.tile');
+tiles.forEach(tile => tile.addEventListener('mouseover', () => {
+    tile.classList.add("hover");
 }));
 
-function toggleClass(e) {
-    this.classList.toggleClass("hover")
-}
