c1cd2520d5a5ee362647dd6d552460ab097fb1eb
euklit <n.lieven@outlook.com>
2023-09-24 19:49:15 +0200

    Set random color on tiles on hover; remove styles.css - only use js for styles;

--- javascript.js
+++ javascript.js
@@ -1,19 +1,47 @@
-const grid = document.createElement('div')
-grid.classList.add('grid');
-
 const body = document.querySelector('body')
+const button = document.createElement('button')
+button.textContent = "Change tile count"
+button.style.cssText = "display: block; margin: 0 auto"
+body.appendChild(button);
+
+gridDimension = 720; //px
+
+createGrid();
 
 
-for (let i=0;i<16*16;i++) { 
-    const tile = document.createElement('div');
-    tile.classList.add("tile");
-    grid.appendChild(tile);
+function createGrid(tilesPerRow = 16) {
+    const grid = document.createElement('div')
+    grid.style.cssText = `width: ${gridDimension}px;
+        height: ${gridDimension}px;
+        display: flex;
+        flex-wrap: wrap;
+        border: 2px solid palevioletred;
+        margin: 0 auto;
+        margin-top: 20px`;
+    grid.classList.add('grid')
+    
+    
+    for (let i=0;i < tilesPerRow*tilesPerRow ;i++) { 
+        const tile = document.createElement('div');
+        tile.style.cssText = `width: ${gridDimension/tilesPerRow}px; height: ${gridDimension/tilesPerRow}px;`;
+
+        grid.appendChild(tile);
+        tile.addEventListener('mouseover', () => changeColor(tile))
+
+
+    }
+
+    body.appendChild(grid);
+
 }
 
-body.appendChild(grid);
 
-const tiles = document.querySelectorAll('.tile');
-tiles.forEach(tile => tile.addEventListener('mouseover', () => {
-    tile.classList.add("hover");
-}));
+button.addEventListener('click', () => {
+    let tilesPerRow = window.prompt("Set tile count per row");
+    body.removeChild(document.querySelector('.grid'));
+    createGrid(tilesPerRow)
+});
 
+function changeColor(tile) {
+    tile.style.background = `rgb(${Math.random()*255}, ${Math.random()*255}, ${Math.random()*255})`
+}
